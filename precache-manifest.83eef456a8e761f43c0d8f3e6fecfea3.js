f64d71a238c70c4fc9b7a21dedbe79911f743f6e
Aswin A V <aswinavofficial@gmail.com>
2020-06-12 22:41:04 +0530

    Updates

---
+++ precache-manifest.83eef456a8e761f43c0d8f3e6fecfea3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7da88e5a0f26bd3a26fb31ad5b94cb1",
+    "url": "/instagram-clone/index.html"
+  },
+  {
+    "revision": "81213c531d4ba75fdd9e",
+    "url": "/instagram-clone/static/css/main.12784269.chunk.css"
+  },
+  {
+    "revision": "271513b6ec9cf92931a7",
+    "url": "/instagram-clone/static/js/2.ad1a0b85.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/instagram-clone/static/js/2.ad1a0b85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81213c531d4ba75fdd9e",
+    "url": "/instagram-clone/static/js/main.7288f131.chunk.js"
+  },
+  {
+    "revision": "b5258bb9a9327c64e89f",
+    "url": "/instagram-clone/static/js/runtime-main.875e94d8.js"
+  }
+]);
