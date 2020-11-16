d845d90a11e5a83c62a522c509cfc6c1823c1eab
Aswin A V <aswinavofficial@gmail.com>
2020-11-16 23:46:56 +0530

    Infinite Scroll

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
 
     const [posts,setPosts] = useState([])
     const [isFetching, setIsFetching] = useState(false);
-
+    const [createdOnBefore,setCreatedOnBefore] = useState(Date.now())
     const requestOptions = {
         method: 'GET',
         headers: { 'Authorization': 'Bearer ' + localStorage.getItem('jwt') },
@@ -16,7 +16,7 @@ const Home = () => {
 
         window.addEventListener('scroll', handleScroll);
 
-        fetch(baseURL + '/post/all', requestOptions)
+        fetch(baseURL + '/post/latest?createdOnBefore=' + createdOnBefore, requestOptions)
             .then(response => response.json())
             .then(data => {
 
@@ -29,7 +29,7 @@ const Home = () => {
                 console.log(data.posts)
                 setPosts(data.posts);
                 console.log(data.posts[0].postedBy.name)
-
+                setCreatedOnBefore(data.posts[data.posts.length -1].createdAt)
                 return () => window.removeEventListener('scroll', handleScroll);
 
             })
@@ -55,21 +55,23 @@ const Home = () => {
 
   function fetchMoreListItems() {
   
-     fetch(baseURL +  '/post/latest', requestOptions)
+     fetch(baseURL +  '/post/latest?createdOnBefore=' + createdOnBefore, requestOptions)
             .then(response => response.json())
             .then(data => {
 
-                console.log(data)
+                console.log(data.posts)
                 
                 if (data.error) {
                     console.log(data.error)
                 }
 
                     console.log("loading more posts : ")
-                    console.log(data)
-                    let newPosts = [...posts,...data]
+                    console.log(data.posts)
+                    let newPosts = [...posts,...data.posts]
 
                     setPosts(newPosts);
+                    let newDate = newPosts[newPosts.length -1].createdAt
+                    setCreatedOnBefore(newDate)
                     console.log("Data state updated")
                     console.log(posts)
                     setIsFetching(false);
